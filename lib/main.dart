445fde5aa50bd32a99934452e6d3ea83f8fd4b0d
Ayush Bhargava <Ayush_Bhargava@us.crawco.com>
2019-12-20 22:18:45 +0530

    add todo screen, list of todos

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,77 @@ class TodoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Todo List',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Todo List')
-        )
+      home: new TodoList(),
+    );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  List<String> _todoItems = [];
+
+  void _addTodoItem(String task) {
+    if (task.length > 0) {
+      setState(() {
+        _todoItems.add(task);
+      });
+    }
+  }
+
+  Widget _buildTodoList() {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        if (index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index]);
+        }
+      },
+    );
+  }
+
+  // Build a single todo item
+  Widget _buildTodoItem(String todoText) {
+    return new ListTile(title: new Text(todoText));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Todo List'),
+      ),
+      body: _buildTodoList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _pushAddTodoScreen,
+        tooltip: 'Add task',
+        child: Icon(Icons.add),
       ),
     );
+  }
+
+  void _pushAddTodoScreen() {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Add a new task'),
+          ),
+          body: TextField(
+            autofocus: true,
+            onSubmitted: (val) {
+              _addTodoItem(val);
+              Navigator.pop(context);
+            },
+            decoration: InputDecoration(
+              hintText: 'Enter something to do..',
+              contentPadding: const EdgeInsets.all(16.0),
+            ),
+          ),
+        );
+      },
+    ));
   }
 }
